1b8e0845c87c7cdc1099b2888019e9e3f5910362
Talia Rodrigues <taliaraodrigues@gmail.com>
2023-04-04 10:28:15 -0300

    mensaje commit

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ alert('Hola Mundo');
 var saludar= 'Hola Como estan?';
 var teclado = 'Muestro teclado';
 console.log(saludar);
-document.write(teclado);
+document.log(teclado);
 
 console.log(saludar + teclado);
